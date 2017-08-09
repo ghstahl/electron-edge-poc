40a07e311e596b99e2406500a4081fd0fe1a8720
ghstahl@gmail.com <ghstahl@gmail.com>
2017-08-09 08:30:30 -0700

    adding in some bootstrap

--- src/renderer.js
+++ src/renderer.js
@@ -9,9 +9,31 @@ window.helloWorld = app.helloWorld;
 
 console.log('helloWorld', app.helloWorld);
 console.log('localFetch', app.localFetch);
+
+let heartBeat = {
+    heart: (data, callback) => {
+        var hb = heartBeat;
+        console.log(data);
+        callback(null, data.key);
+    }
+}
+
 window.boundAsync = {
     fetchLocalJson: (url) => {
         return loadJsonFile(url);
     },
     localFetch: app.localFetch
 }
+
+app.localFetch({
+    url: 'local://v1/command-source/register-heart',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'X-Symc-Fetch-App-Version': '1.0'
+    },
+    body: heartBeat
+}, function(error, result) {
+    if (error) throw error;
+    console.log(result);
+});
